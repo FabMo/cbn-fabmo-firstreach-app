ffff580aa0018f90a1ab600901ee9568a2e76903
tedh-shopbot <ted@shopbottools.com>
2017-02-20 11:56:28 -0500

    finally sort-of working with paper and smoothing

--- js/make_path_paper.js
+++ js/make_path_paper.js
@@ -2,9 +2,11 @@
         var path;
         var last_pos_x = 0;
         var last_pos_y = 0;
-        var pos;
+        var pos, smooth_pt1, smooth_pt2;
         var err;
         var pt_ct = 0, seg_ct = 0, next_ct = 0;
+        var len_here = 0;
+        var smooth_pt = new Point();
 
 
 
@@ -38,22 +40,30 @@
 
                       var to_x = pos.x;
                       var to_y = pos.y;
-                      if (Math.abs(to_x - last_pos_x) > 15 || Math.abs(to_y - last_pos_y) > 15) {
-//                        fabmo.livecodeStart((to_x *.01), (to_y *.01),(err));
+                      if (Math.abs(to_x - last_pos_x) > 2 || Math.abs(to_y - last_pos_y) > 2) {
                         last_pos_x = to_x;
                         last_pos_y = to_y;
                         path.add(pos)
 //                        path.add(event.point);    //original
                         pt_ct++;
-                        if (pt_ct > 3) {
-                          pt_ct = 0;
-                          path.smooth({ type: 'geometric', factor: 0.5, from: seg_ct, to: (seg_ct + 3)});
-                          //next_ct = seg_ct + 3;
-                          //path.segments.select[seg_ct, next_ct];
-                          //path.flatten(.5);
-                          seg_ct += 4;
-                      }
-                        console.log("**nextLoc ", to_x, to_y, pt_ct, seg_ct, path.segments.length);
+  
+                          if (pt_ct > 7) {
+                            pt_ct = 0;
+
+                            path.smooth({ type: 'geometric', factor: 0.7, from: seg_ct, to: (seg_ct + 7)});
+                            seg_ct += 8;
+                            
+                            var dist_now = path.length - len_here;
+
+                            for (i = 0; i < 1; i += 0.1) {
+                              smooth_pt = path.getPointAt(len_here + (i * dist_now));
+                              fabmo.livecodeStart((smooth_pt.x * 0.017), (smooth_pt.y * 0.017),(err));
+                            }
+                            console.log("**nextLoc ", to_x, to_y, pt_ct, seg_ct, path.length);
+
+                            len_here = path.length;
+                          }
+  
                       }          
 
             // Update the content of the text item to show how many
@@ -67,8 +77,9 @@
             path.smooth({ type: 'geometric', factor: 0.5, from: seg_ct, to: (seg_ct + pt_ct)});
             pt_ct = 0;
             seg_ct = 0;
+            len_here = 0;
             var segmentCount = path.segments.length;
-console.log(path);
+            //console.log(path);
             // When the mouse is released, simplify it:
 //            path.simplify(10);
 //            path.smooth({ type: 'geometric', factor: .5 });
