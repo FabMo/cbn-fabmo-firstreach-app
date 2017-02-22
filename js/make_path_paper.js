0f5013f7ba00ce596d81279172fd2091aba602ef
tedh-shopbot <ted@shopbottools.com>
2017-02-22 10:59:21 -0500

    integrated graphic of tool motion

--- js/make_path_paper.js
+++ js/make_path_paper.js
@@ -1,8 +1,9 @@
         var fabmo = new FabMoDashboard();
         var path;
 
-        var last_pos_x = 0;
-        var last_pos_y = 0;
+        var last_pos_x = 0, last_pos_y = 0;
+        var tool_x, tool_y;
+
         var pos, smooth_pt1, smooth_pt2;
         var err;
         var m_rate;
@@ -10,12 +11,44 @@
         var len_here = 0;
         var smooth_pt = new Point();
 
-
-        var textItem = new PointText({
-            content: 'Click and drag to draw a line.',
+        var textItem1 = new PointText({
+            content: 'Segment count/length: ',
             point: new Point(20, 30),
             fillColor: 'black',
         });
+        var textItem2 = new PointText({
+            content: 'Tool Location: ',
+            point: new Point(20, 50),
+            fillColor: 'black',
+        });
+
+
+
+          fabmo.on('status', function(status) {
+            tool_x = status.posx;
+            tool_y = status.posy;
+            circle.position.x = tool_x * 50; 
+            circle.position.y = tool_y * 50; 
+            textItem2.content = 'Tool Location: ' + tool_x.toFixed(3) + ', ' + tool_y.toFixed(3);
+          });
+
+          fabmo.requestStatus();
+//          var circle = new Path.Circle((status.posx * 50),(status.posx * 50), 10);
+          var circle = new Path.Circle(100,100, 10);
+          circle.strokeColor = "black";
+
+
+          fabmo.getConfig(function(err, cfg) {
+            try {
+              if(cfg.machine.envelope) {
+                //display.setExtents(cfg.machine.envelope);
+                //display.gotoExtents(1000);
+              }              
+            } catch(e) {
+              console.error(e);
+            }
+          });
+
 
 
         function onMouseDown(event) {
@@ -46,7 +79,6 @@
                         last_pos_x = to_x;
                         last_pos_y = to_y;
                         path.add(pos)
-//                        path.add(event.point);    //original
                         pt_ct++;
   
                           if (pt_ct > 2* m_rate) {
@@ -59,9 +91,9 @@
 
                             for (i = 0; i < 1; i += 0.1) {
                               smooth_pt = path.getPointAt(len_here + (i * dist_now));
-                              fabmo.livecodeStart((smooth_pt.x * 0.017), (smooth_pt.y * 0.017),(err));
+                              fabmo.livecodeStart((smooth_pt.x * 0.02), (smooth_pt.y * 0.02),(err));
                             }
-                            console.log("**nextLoc ", to_x, to_y, pt_ct, seg_ct, m_rate);
+                            //console.log("**nextLoc ", to_x, to_y, pt_ct, seg_ct, m_rate);
 
                             len_here = path.length;
                           }
@@ -70,7 +102,7 @@
 
             // Update the content of the text item to show how many
             // segments it has:
-            textItem.content = 'Segment count: ' + path.segments.length;
+            textItem1.content = 'Segment count/length: ' + path.segments.length + ' / ' + path.length.toFixed(3);
         }
 
 
@@ -93,5 +125,4 @@
             var newSegmentCount = path.segments.length;
             var difference = segmentCount - newSegmentCount;
             var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
-            textItem.content = path.length + ' long with ' + segmentCount + ' segments ' + m_rate + ' ck?';
         }
